1a776a5ff7435c2371a05e515d590eb8c7c31361
Chase Jensen <soccerchase@gmail.com>
2012-06-26 22:02:45 -0600

    Moved the Yellow page nokogiri script to the nokogiri test file, and switched it to only use mechanize

--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -2,14 +2,27 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-url2 = "http://stgeorge.biz/premium-business-listings/accountants-and-cpas"
+# Test Walmart
 url = "http://www.walmart.com/search/search-ng.do?search_query=batman&ic=16_0&Find=Find&search_constraint=0"
 doc = Nokogiri::HTML(open(url))
-# Title
-# puts doc.at_css("title").text
 
+ 
 doc.css(".item").each do |node|
   title = node.at_css(".prodLink").text 
   price = node.at_css(".camelPrice").text[/\$[0-9\.]+/]
   puts "#{title} - #{price}"
 end
+
+# Test yellowpages
+
+url = "http://www.yellowpages.com/saint-george-ut/tire-dealers"
+doc = Nokogiri::HTML(open(url))
+
+doc.css(".listing_content").each do |business|
+  name = business.css(".url").text
+  puts name
+end
+
+doc.css(".next").each do |button|
+  link = button.css("a")
+  link.end
--- yp_tire_dealers.rb
+++ yp_tire_dealers.rb
@@ -1,22 +1,10 @@
 require 'rubygems'
-require 'nokogiri'
 require 'mechanize'
-require 'open-uri'
 
-# functions
-def get_business()
-end
-url = "http://www.yellowpages.com/saint-george-ut/tire-dealers"
-doc = Nokogiri::HTML(open(url))
+HOME_URL = "http://www.yellowpages.com/saint-george-ut/trends/1"
 
-#first loop 
-doc.css(".listing_content").each do |business|
-  name = business.css(".url").text
-  puts name
-end
-#once loop is finished, click the next button and do it again
-doc.css(".next").each do |button|
-  link = button.css("a")
-  link.end
-   
-click
+mech = Mechanize.new
+mech.user_agent_alias = "Linux Firefox"
+mech.get(HOME_URL)
+
+puts mech.page.links
