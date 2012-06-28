feee4b02547fcd526d8a50647c7c05d8bca8490f
Chase Jensen <soccerchase@gmail.com>
2012-06-27 23:04:26 -0600

    Script can obtain business name, address, city, state, and zip

--- yp_tire_dealers.rb
+++ yp_tire_dealers.rb
@@ -3,11 +3,24 @@ require 'mechanize'
 
 HOME_URL = "http://www.yellowpages.com/saint-george-ut/trends/1"
 
-mech = Mechanize.new
+mech = Mechanize.new 										
 mech.user_agent_alias = "Linux Firefox"
-mech.get(HOME_URL)
+mech.get(HOME_URL)  															
+mech.page.search('.categories-list a').each do |link|       
+  mech.click(link)
+ 
+  mech.page.search('.listing_content').each do |business|	
+  #subpage.download(business.link, 'business.txt')
+  bus = business.css('.url').text.strip
+  address = business.css('.street-address').text.strip
+  
+  city = business.css('.locality').text.strip
+  state = business.css('.region').text.strip
+  zip = business.css('.postal-code').text.strip
 
-#Fix this method
-mech.page.search('.categories-list a').each do |link|
-  puts link
+
+  puts "#{bus} #{address}, #{city}, #{state}, #{zip} \n \n"
+
+  end
+   a = gets
 end
