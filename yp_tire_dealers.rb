21e3e52d93804e18835d750b7a7adbb14e7a1bbb
Chase Jensen <soccerchase@gmail.com>
2012-07-01 19:57:29 -0600

    Now it will click next instead of stopping at 30 businesses

--- yp_tire_dealers.rb
+++ yp_tire_dealers.rb
@@ -2,6 +2,31 @@ require 'rubygems'
 require 'mechanize'
 require 'spreadsheet'
 
+  def next_button(yellowpages, worksheet, row_num)
+    yellowpages.page.search('.next a').each do |next_button|
+      yellowpages.click(next_button)
+      yellowpages.page.search('.listing_content').each do |business|
+        get_business_info(business, worksheet, row_num)
+      end
+    next_button(yellowpages, worksheet, row_num)
+    end
+  end
+
+  def get_business_info(business, worksheet, row_num)
+    @row_num += 1
+    bus = business.at_css('.url').text.strip
+    address = business.css('.street-address').text.strip
+    city = business.css('.locality').text.strip
+    state = business.css('.region').text.strip 
+    zip = business.css('.postal-code').text.strip
+    website = business.at_css('.website-feature')
+    website = website.at_css('a')['href'] if website
+
+    # gets data to excel file
+    row = worksheet.row(@row_num)
+    row.push bus, address, city, state, zip, website
+  end
+
 # Establish connection to site 
 HOME_URL = "http://www.yellowpages.com/saint-george-ut/trends/1"
 yellowpages = Mechanize.new                     
@@ -11,44 +36,32 @@ yellowpages.get(HOME_URL)
 # Initialize excel document
 excel_doc = Spreadsheet::Workbook.new
 a = 0
+
+yellowpages.page.search('.page-navigation a').each do |link|
+  yellowpages.click(link)
+end
 # This iterator goes through each link on the trends/1 page and clicks it
 yellowpages.page.search('.categories-list a').each do |link|       
-  # Prepare new worksheet within excel spreadsheet for current business category
+  if a > 3
+    puts "over #{a}"
+    a = gets
+   end 
+   a += 1
+
   worksheet = excel_doc.create_worksheet
   worksheet.name = link.text
-  worksheet.row(0).concat %w{Company Address City State Zip}
-
+  worksheet.row(0).concat %w{Company Address City State Zip Website}
+  @row_num = 0
+  
   yellowpages.click(link)
   
-  puts '*'
-  row_num = 0
-  
-  # if a >= 50
-  #  puts "over 50"
-  # a = gets
-  # end 
-  # a += 1
+  excel_doc.write 'businesses.xls'
   
   yellowpages.page.search('.listing_content').each do |business|	
-    
-    row_num += 1
-    bus = business.css('.url').text.strip
-    address = business.css('.street-address').text.strip
-    city = business.css('.locality').text.strip
-    state = business.css('.region').text.strip 
-    zip = business.css('.postal-code').text.strip
-    website = business.css('.website-feature').at_css('track-visit-website no-tracks')
-    website.text
-    a = gets
-    # gets data to excel file
-    row = worksheet.row(row_num)
-    row.push bus
-    row.push address
-    row.push city
-    row.push state
-    row.push zip
-    excel_doc.write 'businesses.xls'
+    get_business_info(business, worksheet, @row_num)
   end
 
-
+  next_button(yellowpages, worksheet, @row_num)
 end
+  
+
