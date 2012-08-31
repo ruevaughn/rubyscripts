df93bbd6d829452c74cd4663131befeabb45f1d3
Chase Jensen <soccerchase@gmail.com>
2012-08-30 20:50:40 -0600

    Updated script

--- dex/get_dex_business.rb
+++ dex/get_dex_business.rb
@@ -5,6 +5,9 @@ require 'csv'       # gem install csv
 
 class MyApp
   def initialize 
+    # Delete business.csv if it exists
+    File.delete("businesses.csv") if File.exist?("businesses.csv")
+
     # Open local file with Nokogiri.
     @doc = Nokogiri::HTML(open('stgeorge-categories.html'))
     
@@ -16,35 +19,32 @@ class MyApp
   def run
     # For each 'a' (business) link in local file, send it to Mechanize to do a page search on it.
     @doc.css('a').each do |a|
+
+      CSV.open("businesses.csv", "a") do |csv|
+        csv << [a.text]
+      end
       link = a['href']
       @dexknows.get(link)
       @dexknows.page.search('.listing').each do |business|
         get_business_info(business)
       end
+      puts "Wrote #{a.text}"
     end
   end
 
   # # # # 
   # Functions
-  # # # #
 
-  # I think you know what this function is doing
   def get_business_info(business)
     bus = business.at_css('.details h2 a').text.strip
     address = business.css('h3.address').text.strip
     phone = business.css('.phone').text.strip
     website_node = business.at_css('.tool_web a')
     website = website_node['href'] if website_node
-    
-    # Don't need these, they are in address
-    # city = 'St. George'
-    # state = 'UT' 
-    # zip = '84770'
 
     CSV.open("businesses.csv", "a") do |csv|
       csv << [bus, address, phone, website]
     end
-    puts "wrote #{bus}"
   end
 end
 MyApp.new.run
